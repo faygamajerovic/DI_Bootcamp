06db71ecd14e7ceedfd49a44c930cef18e0bf407
faygamajerovic <saramajerovic@gmail.com>
2021-11-07 08:44:02 -0500

    dc

---
+++ Week_6/Day_1/daily_challenge.sql
@@ -0,0 +1,9 @@
+-- Database: Hollywood
+
+-- DROP DATABASE IF EXISTS "Hollywood";
+
+-- SELECT COUNT(*) as count_actors
+-- FROM actors;
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Clooney', 2);
